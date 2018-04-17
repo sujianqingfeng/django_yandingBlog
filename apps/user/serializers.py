f38b249ac508cfd1f61a0c4c4f9e2f67f08bd4e9
sujian <121116111@qq.com>
2018-04-17 19:25:06 +0800

    update:编辑用户

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -25,6 +25,14 @@ class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=False, help_text='邮件')
     sex = serializers.ChoiceField(choices=User.SEX_TYPE)
 
+    username = serializers.CharField(read_only=True)
+
+
+    def get_sex(self, obj):
+        return obj.get_sex_display()
+
+    sex = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ('username', 'phone', 'sex', 'birthday', 'email', 'desc', 'id', 'icon')
+        fields = ('username', 'phone', 'sex', 'birthday', 'email', 'desc', 'id', 'icon','github','other_link')
