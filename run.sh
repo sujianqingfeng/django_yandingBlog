aa48909ac6930f853deb2f863f2048384c7655c8
sujianqingfeng <h326048518>
2018-06-21 14:56:02 +0800

    update:test ci

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+cd /var/www
 rm -fr django_yandingBlog
 git clone https://github.com/sujianqingfeng/django_yandingBlog.git
 cd django_yandingBlog
 pipenv install --three
 pipenv run python manage.py migrate
+pipenv run python manage.py collectstatic
+sudo killall -9 uwsgi
 pipenv run uwsgi --ini uwsgi.ini
