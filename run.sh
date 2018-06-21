e4268cdbbb6a263e6291a8d26e2bb0ffed36ecbf
sujianqingfeng <h326048518>
2018-06-21 17:02:40 +0800

    update:部署脚本

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 cd /var/www
-rm -fr django_yandingBlog
+
+if [ -d "/var/www/django_yandingBlog/" ];then
+cd django_yandingBlog
+git pull
+else
 git clone https://github.com/sujianqingfeng/django_yandingBlog.git
 cd django_yandingBlog
+fi
+
 pipenv install --three
 pipenv run python manage.py migrate
 pipenv run python manage.py collectstatic
